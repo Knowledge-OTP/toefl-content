cba52e10cb84e37ec21387c67b84e2fcc5ce6201
igormas <igor.masliansky@gmail.com>
2015-10-28 14:17:52 +0200

    cross browser for startsWith function

--- test/crossBrowser/string.js
+++ test/crossBrowser/string.js
@@ -1,4 +1,4 @@
-if(!String.prototype.startsWithx){
+if(!String.prototype.startsWith){
     String.prototype.startsWith = function(startWithVal){
         var strStart = this.substr(0,startWithVal.length);
         return startWithVal === strStart;
